86f63c2c25a0fd809706e850af59092c887827a7
Dimitry Ivanov <2@ivanoff.org.ua>
2023-11-04 23:48:34 +0200

    add colors change

--- typings.d.ts
+++ typings.d.ts
@@ -383,7 +383,11 @@ declare module 'react-native-ico-essential' {
       'zoom-in' |
       'zoom-out';
 
+    type iconColors = ;
+
     type backgroundType = 'circle' | 'rect' | 'button';
+
+    type colorsType = Record<iconColors, string>;
 
     type positionType = 'top' | 'bottom' | 'right' | 'left' |
       'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
@@ -400,6 +404,7 @@ declare module 'react-native-ico-essential' {
       fontSize?: string;
       position?: positionType;
       color?: string;
+      colors: colorsType;
       backgroundColor?: string;
       radius?: number;
     };
@@ -409,6 +414,7 @@ declare module 'react-native-ico-essential' {
       width?: number;
       height?: number;
       color?: string;
+      colors: colorsType;
       stroke?: number;
       background?: backgroundType | backgroundTypeObj;
       badge?: number | string | badgeTypeObj;
